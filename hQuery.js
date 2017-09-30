44801581a8dc333803725e79d5c8bf01475e129f
Hydrogen <fanyangliang@gmail.com>
2017-09-30 12:10:53 +0800

    update document

--- hQuery.js
+++ hQuery.js
@@ -3,12 +3,20 @@
 (function (window, document) {
 
     // for debug
-    let log = console.log.bind(console)
+    const log = console.log.bind(console)
 
-    let w = window,
-        d = document
+    // cache
+    const w = window,
+          d = document
 
-    let H = function (selector, context) {
+    /**
+     * hQuery构造函数，可以直接h()调用
+     * @class
+     * @param {String|DOM} selector - 将querySelectAll的选择器
+     * @param {DOM=} [context = document] - 作为选择器的起点
+     * @return {H} - 返回hQuery对象
+     */
+    function H(selector, context) {
         return new H.prototype.init(selector, context)
     }
 
@@ -43,28 +51,50 @@
 
     H.prototype.init.prototype = H.prototype
 
+    /**
+     * 将hQuery挂载在window对象上
+     * @global
+     */
     w.hQuery = H
 
-
     // selector methods
+
+    /**
+     * 以当前对象的第一个节点为根节点，返回符合选择器的子节点
+     * @param {string} selector 选择器
+     * @return {H} 全新的hQuey对象
+     */
     H.prototype.find = function (selector) {
         let ret = new H(selector, this[0])
         ret.prevObject = this
         return ret
     }
 
+    /**
+     * 返回被选中的第一个节点
+     * @return {H} 全新的hQuey对象
+     */
     H.prototype.first = function () {
         let ret = new H(this[0])
         ret.prevObject = this
         return ret
     }
 
+    /**
+     * 返回被选中的最后一个节点
+     * @return {H} 全新的hQuery对象
+     */
     H.prototype.last = function () {
         let ret = new H(this[this.length-1])
         ret.prevObject = this
         return ret
     }
 
+    /**
+     * 返回被选中的第n个节点
+     * @param {number} num - 从0开始指定第num个节点，可接受负数
+     * @return {H} 全新的hQuery对象
+     */
     H.prototype.eq = function (num) {
         num = num > 0 ? num : this.length - num
         let ret = new H(this[num])
@@ -72,12 +102,20 @@
         return ret
     }
 
+    /**
+     * 返回第一个节点的父节点
+     * @return {H} 全新的hQuery对象
+     */ 
     H.prototype.parent = function () {
         let ret = new H(this[0].parentNode)
         ret.prevObject = this
         return ret
     }
 
+    /**
+     * 返回第一个节点的所有子节点
+     * @return {H} 全新的hQuery对象
+     */     
     H.prototype.children = function (num) {
         let ret;
         let children = this[0].children
@@ -94,7 +132,11 @@
         ret.prevObject = this
         return ret
     }
-
+    
+    /**
+     * 返回所有节点的下一个兄弟节点集合，忽略文本节点
+     * @return {H} 全新的hQuery对象
+     */  
     H.prototype.next = function () {
         let index = 0;
         let ret = new H()
@@ -113,6 +155,10 @@
         return ret
     }
 
+    /**
+     * 返回所有节点的前一个兄弟节点集合，忽略文本节点
+     * @return {H} 全新的hQuery对象
+     */  
     H.prototype.prev = function () {
         let index = 0;
         let ret = new H()
@@ -130,12 +176,23 @@
         return ret
     }
 
-    H.prototype.slice = function() {
+    /**
+     * 返回[start, end)的节点集合
+     * @param {number} start - 起点
+     * @param {number} end - 终点
+     * @return {H} 全新的hQuery对象
+     */  
+    H.prototype.slice = function (start, end) {
         let ret = [].slice.apply(this, arguments)
         ret.prevObject = this
         return ret
     }
 
+    /**
+     * 返回满足筛选函数的节点
+     * @param {function} fn - 用作筛选的函数，应返回boolean值
+     * @return {H} 全新的hQuery对象
+     */  
     H.prototype.filter = function (fn) {
         let index = 0;
         let ret = new H()
@@ -150,11 +207,23 @@
         return ret
     }
 
+    /**
+     * 回溯到链上的上一个hQuery对象
+     * @return {H} 全新的hQuery对象
+     * @example
+     * h('#id').find('.class').end() // 会被回溯到h('#id')
+     */  
     H.prototype.end = function () {
         return this.prevObject ? this.prevObject : this
     }
 
     // html methods
+
+    /**
+     * 返回或者设置节点的html
+     * @param {string=} html - 将要设置的html值
+     * @return {string|H} 返回html内容或者自身
+     */  
     H.prototype.html = function (html) {
         for (let i = 0; i < this.length; i++) {
             if (html === undefined) {
@@ -165,6 +234,11 @@
         return this
     }
 
+    /**
+     * 返回或者设置节点的text
+     * @param {string=} text - 将要设置的text值
+     * @return {string|H} 返回text内容或者自身
+     */  
     H.prototype.text = function (text) {
         for (let i = 0; i < this.length; i++) {
             if (text === undefined) {
@@ -175,6 +249,11 @@
         return this
     }
 
+    /**
+     * 返回或者设置节点的value
+     * @param {string=} value - 将要设置的value值
+     * @return {string|H} 返回value内容或者自身
+     */  
     H.prototype.val = function (val) {
         for (let i = 0; i < this.length; i++) {
             if (val === undefined) {
@@ -185,6 +264,12 @@
         return this
     }
 
+    /**
+     * 返回或者设置节点的属性
+     * @param {string|object} attr - 想取得或者将要设置的attr值，可直接传入{ attr: value }的对象
+     * @param {string=} value - 要设置的attr值
+     * @return {string|H} 返回属性值或者自身
+     */  
     H.prototype.attr = function (attr, val) {
         for (let i = 0; i < this.length; i++) {
             if (typeof attr === 'string') {
@@ -201,6 +286,11 @@
         return this
     }
 
+    /**
+     * 在节点的子节点末尾添加html
+     * @param {string|DOM} - 想要添加的html或者节点，DOM可以传入多个
+     * @return {H} - 返回自身
+     */
     H.prototype.append = function (html) {
         for (let i = 0; i < this.length; i++) {
             if (typeof html === 'string') {
@@ -213,6 +303,11 @@
         return this
     }
     
+    /**
+     * 在节点的子节点开头添加html
+     * @param {string|DOM} - 想要添加的html或者节点，DOM可以传入多个
+     * @return {H} - 返回自身
+     */
     H.prototype.prepend = function (html) {
         for (let i = 0; i < this.length; i++) {
             if (typeof html === 'string') {
@@ -225,6 +320,10 @@
         return this
     }
 
+    /**
+     * 删除节点
+     * @return {H} - 返回自身
+     */
     H.prototype.remove = function () {
         for (let i = 0; i < this.length; i++) {
             this[i].remove()
@@ -232,6 +331,11 @@
         return this
     }
 
+    /**
+     * 替代当前节点
+     * @param {string|DOM} - 用以替换的html或者节点，DOM可以传入多个
+     * @return {H} - 返回自身
+     */
     H.prototype.replaceWith = function (html) {
         for (let i = 0; i < this.length; i++) {
             if (typeof html === 'string') {
@@ -243,6 +347,10 @@
         return this
     }
 
+    /**
+     * 隐藏节点
+     * @return {H} - 返回自身
+     */
     H.prototype.hide = function () {
         for (let i = 0; i < this.length; i++) {
             this[i].style.display = 'none'
@@ -250,6 +358,10 @@
         return this
     }
 
+    /**
+     * 显示节点
+     * @return {H} - 返回自身
+     */
     H.prototype.show = function () {
         for (let i = 0; i < this.length; i++) {
             this[i].style.display = ''
@@ -257,6 +369,10 @@
         return this
     }
 
+    /**
+     * 切换隐藏和显示的状态
+     * @return {H} - 返回自身
+     */
     H.prototype.toggle = function () {
         for (let i = 0; i < this.length; i++) {
             this[i].style.display = this[i].style.display === 'none' ? '' : 'none'
@@ -266,7 +382,14 @@
 
 
     // css methods
-    H.prototype.css = function (attr, val) {
+
+    /**
+     * 返回或者设置节点的css
+     * @param {string|object} attr - 想取得或者将要设置的css值，可直接传入{ attr: value }的对象
+     * @param {string=} value - 要设置的css值
+     * @return {string|H} 返回属性值或者自身
+     */  
+    H.prototype.css = function (attr, val) {c
         for (let i = 0; i < this.length; i++) {
             if (typeof attr === 'string') {
                 if (arguments.length === 1) {
@@ -282,10 +405,20 @@
         return this
     }
 
+    /**
+     * 检测节点是否有该class
+     * @param {string} className - 检测的类名
+     * @return {boolean}
+     */  
     H.prototype.hasClass = function (className) {
         return this[0].classList.contains(className)
     }
 
+    /**
+     * 为节点添加class
+     * @param {string} className - 想增加的类名
+     * @return {H} 返回自身
+     */  
     H.prototype.addClass = function (className) {
         for (let i = 0; i < this.length; i++) {
             this[i].classList.add(className)
@@ -293,6 +426,11 @@
         return this
     }
 
+    /**
+     * 为节点去除class
+     * @param {string} className - 想去除的类名
+     * @return {H} 返回自身
+     */  
     H.prototype.removeClass = function (className) {
         for (let i = 0; i < this.length; i++) {
             this[i].classList.remove(className)
@@ -300,6 +438,12 @@
         return this
     }
 
+    /**
+     * 切换class
+     * @param {string} className - 想切换的类名
+     * @param {boolen=} force - 可选切换方式，true表示增加，false表示去除
+     * @return {H} 返回自身
+     */  
     H.prototype.toggleClass = function (className, force) {
         /*
             When a second argument is present:
@@ -307,12 +451,19 @@
             and if it evaluates to false, remove it.
          */
         for (let i = 0; i < this.length; i++) {
-            this[i].classList.toggle(className, force)
+            this[i].classList.toggle(...arguments)
         }
         return this
     }
 
     // Event Listener methods
+
+    /**
+     * 增加绑定事件
+     * @param {string} event - 事件名
+     * @param {function} handler - 回调函数，记得处理this绑定
+     * @return {H} 返回自身
+     */  
     H.prototype.on = function (event, handler) {
         for (let i = 0; i < this.length; i++) {
             this[i].addEventListener(event, handler);
@@ -320,6 +471,12 @@
         return this
     }
 
+    /**
+     * 去除绑定事件
+     * @param {string} event - 事件名
+     * @param {function} handler - 回调函数
+     * @return {H} 返回自身
+     */  
     H.prototype.off = function (event, handler) {
         for (let i = 0; i < this.length; i++) {
             this[i].removeEventListener(event, handler);
@@ -327,6 +484,12 @@
         return this
     }
 
+    /**
+     * 增加绑定事件，该事件在触发一次后会被移除
+     * @param {string} event - 事件名
+     * @param {function} handler - 回调函数，记得处理this绑定
+     * @return {H} 返回自身
+     */  
     H.prototype.one = function (event, handler) {
         for (let i = 0; i < this.length; i++) {
             let fn = (e) => {
@@ -340,6 +503,12 @@
 
 
     // other methods
+
+    /**
+     * 对每一个节点调用处理函数
+     * @param {function} fn - 处理函数，如果返回false会中断后续执行
+     * @return {H} 返回自身
+     */  
     H.prototype.each = function (fn) {
         for (let i = 0; i < this.length; i++) {
             let val = fn.call(this[i], i, this[i])
@@ -348,6 +517,12 @@
         return this
     }
 
+    /**
+     * 在hQuery的原型链上扩展新的方法
+     * @param {name} name - 新增的方法名
+     * @param {function} fn - 新增的方法
+     * @return {H} 返回自身
+     */  
     H.extend = function (name, fn) {
         if (arguments.length === 2) {
             H.prototype[name] = fn
